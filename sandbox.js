977e03e231ab5dc33d14efdbda597ba424353ad5
kmb501 <krb501@gmail.com>
2023-02-09 19:39:14 -0600

    practice html and javascript

---
+++ sandbox.js
@@ -0,0 +1,27 @@
+/*alert('hello, world');
+the semi-colon is like the full stop (period) in a sentence. 
+It's best practice to use it*/
+let age = 25; 
+let year = 2019
+console.log(age, year);
+age = 30 
+console.log(age);
+const points = 100 
+console.log(points);
+/* you cannot say "points = 50" because it's been assigned as a constant 
+ var is an older way of creating a variable in JS
+camelCase--cannot start with a number can use any letter or number, plus underscores and dollar signs, except keywords.
+Data Types 
+Numbers--1,2,3.14,300
+Strings--"hi, how are you"
+Booleans--true/false 
+Nulls--value with no value 
+Undefined--variables that have ,not been defined
+Object--complex data structures--arrays, dates, literals, etc.  
+Symbol--used with objects  */
+console.log('Hello, World');
+let email = 'krb501@gmail.com';
+console.log(email); 
+let firstName = 'Kendra';
+let lastName = 'Brooks';
+let fullName = fistName+lastName;
